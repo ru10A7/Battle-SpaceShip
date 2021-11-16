0f1539af87e3387ce3050321988b3ac92e786036
ru10A7 <56973864+ru10A7@users.noreply.github.com>
2021-11-16 12:29:43 +0200

    'add the movement of the spaceships'

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ WIDTH, HEIGHT = 900, 500
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-pygame.display.set_caption("First Game by @Ru10")
+pygame.display.set_caption("Battle Space Ship by @Ru10")
 
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+BORDER = pygame.Rect(WIDTH/2-5, 0, 10, HEIGHT)
 
 FPS = 60
 VEL = 5
@@ -25,9 +28,32 @@ RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
 
 def Draw_window(red, yellow):
     WIN.fill(WHITE)
+    pygame.draw.rect(WIN, BLACK, BORDER)
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
+
+
+def yellow_handle_movement(keys_pressed, yellow):
+    if keys_pressed[pygame.K_a] and yellow.x - VEL > 0:  # LEFT
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x:  # RIGHT
+        yellow.x += VEL
+    if keys_pressed[pygame.K_w] and yellow.y - VEL > 0:  # UP
+        yellow.y -= VEL
+    if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT-20:  # DOWN
+        yellow.y += VEL
+
+
+def red_handle_movement(keys_pressed, red):
+    if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x + BORDER.width:  # LEFT
+        red.x -= VEL
+    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < WIDTH:  # RIGHT
+        red.x += VEL
+    if keys_pressed[pygame.K_UP] and red.y - VEL > 0:  # UP
+        red.y -= VEL
+    if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT-20:  # DOWN
+        red.y += VEL
 
 
 def main():
@@ -43,8 +69,8 @@ def main():
                 run = False
 
         keys_pressed = pygame.key.get_pressed()
-        if keys_pressed[pygame.K_a]:  # LEFT
-            yellow.x -= VEL
+        yellow_handle_movement(keys_pressed, yellow)
+        red_handle_movement(keys_pressed, red)
 
         Draw_window(red, yellow)
 
